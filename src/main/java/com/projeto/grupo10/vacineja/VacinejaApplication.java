6ecf81f69734b96123f9ac67479b325d5de488f6
Caio Davi <caio.silva@ccc.ufcg.edu.br>
2021-04-26 14:03:52 -0300

    primeiro commit

---
+++ src/main/java/com/projeto/grupo10/vacineja/VacinejaApplication.java
@@ -0,0 +1,13 @@
+package com.projeto.grupo10.vacineja;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VacinejaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VacinejaApplication.class, args);
+	}
+
+}
