96dd1ca0a8b110464eeae5ac5add700d95e673db
Holliver de Oliveira Costa <holliver.costa@ccc.ufcg.edu.br>
2021-05-04 10:28:50 -0300

    Revert "fiz a verificação do email e alterei o properties do banco de dados"

--- src/main/java/com/projeto/grupo10/vacineja/util/ErroCidadao.java
+++ src/main/java/com/projeto/grupo10/vacineja/util/ErroCidadao.java
@@ -10,7 +10,6 @@ public class ErroCidadao {
             "como administrador";
     static final String USUARIO_NAO_ENCONTRADO = "O usuário não foi encontrado";
     static final String CIDADAO_CADASTRADO = "Cidadão com cpf %s já está cadastrado";
-    static final String EMAIL_INVALIDO = "Email inválido";
 
     public static ResponseEntity<String> erroSemPermissaoFuncionario(String usuario) {
         return new ResponseEntity<String>(String.format(ErroCidadao.USUARIO_SEM_PERMISSAO_FUNCIONARIO, usuario),
@@ -30,9 +29,4 @@ public class ErroCidadao {
 				String.format(ErroCidadao.CIDADAO_CADASTRADO, cpf), 
 				HttpStatus.NOT_ACCEPTABLE);
 	}
-    public static ResponseEntity<String> erroEmailInvalido() {
-        return new ResponseEntity<String>(
-                String.format(ErroCidadao.EMAIL_INVALIDO),
-                HttpStatus.NOT_ACCEPTABLE);
-    }
 }
