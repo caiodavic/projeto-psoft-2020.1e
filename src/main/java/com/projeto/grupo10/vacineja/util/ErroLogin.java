19eed687c30fae46a5dd1c4096f0762fc796664c
Holliver <holliver.costa@ccc.ufcg.edu.br>
2021-05-05 19:47:30 -0300

    consertei as classes que lançavam erros com tipo string e mudei para lançar com o custom type error

--- src/main/java/com/projeto/grupo10/vacineja/util/ErroLogin.java
+++ src/main/java/com/projeto/grupo10/vacineja/util/ErroLogin.java
@@ -12,19 +12,19 @@ public class ErroLogin {
     static final String ERRO_TOKEN_INVALIDO = "Token invalido ou expirado!";
 
 
-    public static ResponseEntity<String> erroLoginSenhaUsuarioErrado() {
-        return new ResponseEntity<String>(ErroLogin.ERRO_AUTENTICAR_LOGIN_USUARIO_SENHA_ERRADO, HttpStatus.FORBIDDEN);
+    public static ResponseEntity<CustomErrorType> erroLoginSenhaUsuarioErrado() {
+        return new ResponseEntity<CustomErrorType>( new CustomErrorType(ErroLogin.ERRO_AUTENTICAR_LOGIN_USUARIO_SENHA_ERRADO), HttpStatus.FORBIDDEN);
     }
 
-    public static ResponseEntity<String> erroLoginNaoAutorizadoFuncionario() {
-        return new ResponseEntity<String>(ErroLogin.ERRO_AUTENTICAR_LOGIN_FUNCIONARIO, HttpStatus.FORBIDDEN);
+    public static ResponseEntity<CustomErrorType> erroLoginNaoAutorizadoFuncionario() {
+        return new ResponseEntity<CustomErrorType>(new CustomErrorType(ErroLogin.ERRO_AUTENTICAR_LOGIN_FUNCIONARIO), HttpStatus.FORBIDDEN);
     }
 
-    public static ResponseEntity<String> erroLoginNaoAutorizadoAdministrador() {
-        return new ResponseEntity<String>(ErroLogin.ERRO_AUTENTICAR_LOGIN_ADMINISTRADOR, HttpStatus.FORBIDDEN);
+    public static ResponseEntity<CustomErrorType> erroLoginNaoAutorizadoAdministrador() {
+        return new ResponseEntity<CustomErrorType>(new CustomErrorType(ErroLogin.ERRO_AUTENTICAR_LOGIN_ADMINISTRADOR), HttpStatus.FORBIDDEN);
     }
 
-    public static ResponseEntity<String> erroTokenInvalido(){
-        return new ResponseEntity<String>(ErroLogin.ERRO_TOKEN_INVALIDO, HttpStatus.FORBIDDEN);
+    public static ResponseEntity<CustomErrorType> erroTokenInvalido(){
+        return new ResponseEntity<CustomErrorType>(new CustomErrorType(ErroLogin.ERRO_TOKEN_INVALIDO), HttpStatus.FORBIDDEN);
     }
 }
