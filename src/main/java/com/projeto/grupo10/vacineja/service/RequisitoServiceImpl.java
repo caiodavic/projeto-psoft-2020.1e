95453b972a684543a49c82b28932e52b4eeb4d6e
Artur Souza <artur.souza@ccc.ufcg.edu.br>
2021-05-07 23:33:50 -0300

    adicionando individualmente

--- src/main/java/com/projeto/grupo10/vacineja/service/RequisitoServiceImpl.java
+++ src/main/java/com/projeto/grupo10/vacineja/service/RequisitoServiceImpl.java
@@ -8,7 +8,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
-import static com.projeto.grupo10.vacineja.util.PadronizaString.removeAcentos;
+import static com.projeto.grupo10.vacineja.util.PadronizaString.padronizaString;
 
 
 public class RequisitoServiceImpl implements RequisitoService{
@@ -36,7 +36,7 @@ public class RequisitoServiceImpl implements RequisitoService{
             throw new IllegalArgumentException(String.format("Requisito %s já cadastrado",requisito.getRequisito()));
         }
 
-        Requisito novaComorbidade = new RequisitoComorbidade(requisito.getIdade(), removeAcentos(requisito.getRequisito()));
+        Requisito novaComorbidade = new RequisitoComorbidade(requisito.getIdade(), padronizaString(requisito.getRequisito()));
         requisitoRepository.save(novaComorbidade);
     }
 
@@ -47,7 +47,7 @@ public class RequisitoServiceImpl implements RequisitoService{
             throw new IllegalArgumentException(String.format("Requisito %s já cadastrado",requisito.getRequisito()));
         }
 
-        Requisito novaComorbidade = new RequisitoComorbidade(requisito.getIdade(), removeAcentos(requisito.getRequisito()));
+        Requisito novaComorbidade = new RequisitoComorbidade(requisito.getIdade(), padronizaString(requisito.getRequisito()));
         requisitoRepository.save(novaComorbidade);
     }
 
