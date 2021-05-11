507cc73b6d8288434b3e60ea8f14cd1a67d18432
caetanobca <caetano.albuquerque@ccc.ufcg.edu.br>
2021-05-10 21:48:16 -0300

    revertendo main

--- src/main/java/com/projeto/grupo10/vacineja/repository/CidadaoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.projeto.grupo10.vacineja.repository;
-
-import com.projeto.grupo10.vacineja.model.usuario.Cidadao;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CidadaoRepository extends JpaRepository<Cidadao, String> {
-}
--- src/main/java/com/projeto/grupo10/vacineja/repository/FuncionarioGovernoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.projeto.grupo10.vacineja.repository;
-
-import com.projeto.grupo10.vacineja.model.usuario.FuncionarioGoverno;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FuncionarioGovernoRepository extends JpaRepository<FuncionarioGoverno, String> {
-}
