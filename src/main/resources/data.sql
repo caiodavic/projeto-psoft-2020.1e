c14716b75c50703e2c39f161dd910d75f55c6ecf
caiodavic <53905948+caiodavic@users.noreply.github.com>
2021-05-15 01:19:14 -0300

    Update data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -7,5 +7,5 @@
 -- }
 --  */
 --
--- INSERT INTO cartao_vacina(cartao_sus, situacao) VALUES ('000000000000000','VACINACAOFINALIZADA');
--- INSERT INTO cidadao (email,cpf,data_nascimento,nome,senha,cartao_vacina_cartao_sus) VALUES ('admin@admin','00000000000',date'1900-05-28','Administrador','admin','000000000000000');
+INSERT INTO cartao_vacina(cartao_sus, situacao) VALUES ('000000000000000','VACINACAOFINALIZADA');
+INSERT INTO cidadao (email,cpf,data_nascimento,nome,senha,cartao_vacina_cartao_sus) VALUES ('admin@admin','00000000000',date'1900-05-28','Administrador','admin','000000000000000');
